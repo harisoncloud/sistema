57251223b26a1e1a65298121c43d2fb727854021
kaaci <kaaci@unochapeco.edu.br>
2021-05-28 13:18:00 -0300

    nao sei

--- softgun/entradas/models.py
+++ softgun/entradas/models.py
@@ -1,10 +1,11 @@
+from softgun.pessoa.models import pessoa
 from django.db import models
-# Create your models here.
 
+# Create your models here.
 class entrada(models.Model):
     cod = models.AutoField(primary_key=True)
     numeronf = models.IntegerField()
     dataemissao = models.DateField()
     dataentrada = models.DateField()
-    codfornecedor = models.IntegerField()
+    codfornecedor = models.ForeignKey(pessoa.cod)
 

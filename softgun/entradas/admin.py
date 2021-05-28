2eff8ac441daefebe16bcb72651bfa73083a0f05
kaaci <kaaci@unochapeco.edu.br>
2021-05-28 09:01:58 -0300

    -

--- softgun/entradas/admin.py
+++ softgun/entradas/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import entradas
+from .models import entrada
 
 # Register your models here.
 
-admin.site.register(entradas)
+admin.site.register(entrada)
